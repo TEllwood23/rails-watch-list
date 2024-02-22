353e28006a4185f4a4c419cebce87e56e72eacb1
Thomas Ellwood <taellwood@gmail.com>
2024-02-22 12:32:55 +0000

    added name column to Lists

---
+++ db/migrate/20240222122903_add_part_nameto_lists.rb
@@ -0,0 +1,5 @@
+class AddPartNametoLists < ActiveRecord::Migration[7.1]
+  def change
+    add_column :lists, :name, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_22_121803) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_22_122903) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "lists", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
   end
 
   create_table "movies", force: :cascade do |t|
