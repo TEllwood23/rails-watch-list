b3fa3284ec3b237d0a038e8a182426b05fb81f45
Thomas Ellwood <taellwood@gmail.com>
2024-02-22 15:30:09 +0000

    completed lists

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -7,14 +7,21 @@ class ListsController < ApplicationController
 
 
   def show
-    @list = List.find(params[:list_id])
+    @list = List.find(params[:id])
   end
 
   def new
-    @list = List.find(params[:list_id])
-    @movie = Movie.new
+    @list = List.new
   end
 
+  def create
+    @list = List.new(list_params)
+    @list.save
+  end
+end
 
+private
 
+def list_params
+  params.require(:list).permit(:name)
 end
