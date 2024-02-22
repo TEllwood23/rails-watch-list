87759940e00e2b2a82ab431a254ac3d6f4f8204e
Thomas Ellwood <taellwood@gmail.com>
2024-02-22 14:57:05 +0000

    seeded Lists

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,20 @@
 class ListsController < ApplicationController
+
+
+  def index
+    @lists = List.all
+  end
+
+
+  def show
+    @list = List.find(params[:list_id])
+  end
+
+  def new
+    @list = List.find(params[:list_id])
+    @movie = Movie.new
+  end
+
+
+
 end
