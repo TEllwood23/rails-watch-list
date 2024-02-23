5b46920af38dbf56b4c1a85ab5062783562a9a1a
Thomas Ellwood <taellwood@gmail.com>
2024-02-23 11:22:31 +0000

    added delete bookmark buttons

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,11 @@ Rails.application.routes.draw do
   # resources :movies do
   #   end
 
+  root "lists#index"
     resources :lists, only: [:index, :new, :create, :show, :destroy] do
       resources :bookmarks, only: [:new, :create]
     end
+    resources :bookmarks, only: [:destroy]
 
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
